8eaa51155663b3b4775327d1ce203bd8bd1f3f5a
Mohamad Nizar Daouk <40494654+Nizar1999@users.noreply.github.com>
2023-08-01 21:12:00 +0300

    Refactored  rendering_context from static class to namespace

--- SolarSystemSim/src/cpp/rendering_context.cpp
+++ SolarSystemSim/src/cpp/rendering_context.cpp
@@ -4,141 +4,149 @@
 
 #include "rendering_context.h"
 
-Shader* RenderingContext::m_shader = nullptr;
-Shader* RenderingContext::m_skyboxShader = nullptr;
-Shader* RenderingContext::m_orbitShader = nullptr;
-
-Camera RenderingContext::m_camera(glm::vec3(0.0f, 100.0f, 150.0f));
-
-unsigned int RenderingContext::m_width;
-unsigned int RenderingContext::m_height;
-
-GLFWwindow* RenderingContext::m_window = nullptr;
-
-float RenderingContext::m_deltaTime = 0.0f;
-float RenderingContext::m_lastFrame = 0.0f;
-
-bool RenderingContext::m_firstMouse = true;
-bool RenderingContext::m_focusGUI = false;
-float RenderingContext::m_lastX;
-float RenderingContext::m_lastY;
-
-void RenderingContext::initialize(unsigned int width, unsigned int height)
+namespace RenderingContext
 {
-	m_width = width;
-	m_height = height;
+	Shader* m_shader = nullptr;
+	Shader* m_skyboxShader = nullptr;
+	Shader* m_orbitShader = nullptr;
 
-	m_lastX = static_cast<float>(m_width) / 2.0f;
-	m_lastY = static_cast<float>(m_height) / 2.0f;
+	Camera m_camera(glm::vec3(0.0f, 100.0f, 150.0f));
 
-	if (!glfwInit())
+	unsigned int m_width;
+	unsigned int m_height;
+
+	GLFWwindow* m_window = nullptr;
+
+	float m_deltaTime = 0.0f;
+	bool m_focusGUI = false;
+	float m_lastX;
+	float m_lastY;
+
+	namespace
 	{
-		std::cerr << "Failed to initialize GLFW" << std::endl;
+		float m_lastFrame = 0.0f;
+		bool m_firstMouse = true;
+
+		void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+		{
+			glViewport(0, 0, width, height);
+		}
+
+		void errorCallback(int error, const char* description)
+		{
+			std::cerr << "GLFW Error: " << description << std::endl;
+		}
+
+		void mouseCallback(GLFWwindow* window, double xposIn, double yposIn)
+		{
+			if (m_focusGUI)
+			{
+				glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+				return;
+			}
+			glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+			float xpos = static_cast<float>(xposIn);
+			float ypos = static_cast<float>(yposIn);
+
+			if (m_firstMouse)
+			{
+				m_lastX = xpos;
+				m_lastY = ypos;
+				m_firstMouse = false;
+			}
+
+			float xoffset = xpos - m_lastX;
+			float yoffset = m_lastY - ypos; // reversed since y-coordinates go from bottom to top
+			m_lastX = xpos;
+			m_lastY = ypos;
+
+			m_camera.ProcessMouseMovement(xoffset, yoffset);
+		}
+
+		void scrollCallback(GLFWwindow* window, double xoffset, double yoffset)
+		{
+			if (m_focusGUI)
+				return;
+			m_camera.ProcessMouseScroll(yoffset);
+		}
 	}
 
-	glfwSetErrorCallback(errorCallback);
-
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	m_window = glfwCreateWindow(m_width, m_height, "OpenGL Renderer", nullptr, nullptr);
-	if (!m_window)
+	void initialize(unsigned int width, unsigned int height)
 	{
-		std::cerr << "Failed to create GLFW window" << std::endl;
-		cleanup();
-	}
-	glfwMakeContextCurrent(m_window);
+		m_width = width;
+		m_height = height;
 
-	glfwSetFramebufferSizeCallback(m_window, framebufferSizeCallback);
-	glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-	glfwSetCursorPosCallback(m_window, mouseCallback);
-	glfwSetScrollCallback(m_window, scrollCallback);
+		m_lastX = static_cast<float>(m_width) / 2.0f;
+		m_lastY = static_cast<float>(m_height) / 2.0f;
 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		cleanup();
-	}
+		if (!glfwInit())
+		{
+			std::cerr << "Failed to initialize GLFW" << std::endl;
+		}
 
-	m_shader = new Shader("./src/shaders/shader.vs", "./src/shaders/shader.fs");
-	m_skyboxShader = new Shader("./src/shaders/shader_skybox.vs", "./src/shaders/shader_skybox.fs");
-	m_orbitShader = new Shader("./src/shaders/shader.vs", "./src/shaders/shader_orbit.fs");
+		glfwSetErrorCallback(errorCallback);
 
-	glEnable(GL_DEPTH_TEST);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Setup Dear ImGui context
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+		m_window = glfwCreateWindow(m_width, m_height, "OpenGL Renderer", nullptr, nullptr);
+		if (!m_window)
+		{
+			std::cerr << "Failed to create GLFW window" << std::endl;
+			cleanup();
+		}
+		glfwMakeContextCurrent(m_window);
 
-	// Setup Platform/Renderer backends
-	ImGui_ImplGlfw_InitForOpenGL(m_window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
-	ImGui_ImplOpenGL3_Init();
-}
+		glfwSetFramebufferSizeCallback(m_window, framebufferSizeCallback);
+		glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+		glfwSetCursorPosCallback(m_window, mouseCallback);
+		glfwSetScrollCallback(m_window, scrollCallback);
 
-void RenderingContext::updateDeltaTime()
-{
-	auto currentFrame = static_cast<float>(glfwGetTime());
-	m_deltaTime = currentFrame - m_lastFrame;
-	m_lastFrame = currentFrame;
-}
+		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+		{
+			std::cout << "Failed to initialize GLAD" << std::endl;
+			cleanup();
+		}
 
-void RenderingContext::cleanup()
-{
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
+		m_shader = new Shader("./src/shaders/shader.vs", "./src/shaders/shader.fs");
+		m_skyboxShader = new Shader("./src/shaders/shader_skybox.vs", "./src/shaders/shader_skybox.fs");
+		m_orbitShader = new Shader("./src/shaders/shader.vs", "./src/shaders/shader_orbit.fs");
 
-	delete m_shader;
-	delete m_skyboxShader;
-	delete m_orbitShader;
-	glfwTerminate();
-}
+		glEnable(GL_DEPTH_TEST);
 
-void RenderingContext::framebufferSizeCallback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
-
-void RenderingContext::errorCallback(int error, const char* description)
-{
-	std::cerr << "GLFW Error: " << description << std::endl;
-}
-
-void RenderingContext::mouseCallback(GLFWwindow* window, double xposIn, double yposIn)
-{
-	if (m_focusGUI)
-	{
-		glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-		return;
-	}
-	glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-	float xpos = static_cast<float>(xposIn);
-	float ypos = static_cast<float>(yposIn);
-
-	if (m_firstMouse)
-	{
-		m_lastX = xpos;
-		m_lastY = ypos;
-		m_firstMouse = false;
+		setupGui();
 	}
 
-	float xoffset = xpos - m_lastX;
-	float yoffset = m_lastY - ypos; // reversed since y-coordinates go from bottom to top
-	m_lastX = xpos;
-	m_lastY = ypos;
+	void setupGui()
+	{
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGuiIO& io = ImGui::GetIO();
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+		ImGui_ImplGlfw_InitForOpenGL(m_window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+		ImGui_ImplOpenGL3_Init();
+	}
 
-	m_camera.ProcessMouseMovement(xoffset, yoffset);
-}
+	void updateDeltaTime()
+	{
+		auto currentFrame = static_cast<float>(glfwGetTime());
+		m_deltaTime = currentFrame - m_lastFrame;
+		m_lastFrame = currentFrame;
+	}
 
-void RenderingContext::scrollCallback(GLFWwindow* window, double xoffset, double yoffset)
-{
-	if (m_focusGUI)
-		return;
-	m_camera.ProcessMouseScroll(yoffset);
+	void cleanup()
+	{
+		ImGui_ImplOpenGL3_Shutdown();
+		ImGui_ImplGlfw_Shutdown();
+		ImGui::DestroyContext();
+
+		delete m_shader;
+		delete m_skyboxShader;
+		delete m_orbitShader;
+		glfwTerminate();
+	}
 }
 
--- SolarSystemSim/src/h/rendering_context.h
+++ SolarSystemSim/src/h/rendering_context.h
@@ -5,33 +5,27 @@
 #include "camera.h"
 #include "shader.h"
 
-class RenderingContext
+namespace RenderingContext
 {
-public:
-	static void initialize(unsigned int width, unsigned int height);
-	static void updateDeltaTime();
-	static void cleanup();
+	extern Shader* m_shader;
+	extern Shader* m_skyboxShader;
+	extern Shader* m_orbitShader;
 
-	static Shader* m_shader;
-	static Shader* m_skyboxShader;
-	static Shader* m_orbitShader;
-	static Camera m_camera;
-	static unsigned int m_width;
-	static unsigned int m_height;
+	extern Camera m_camera;
 
-	static GLFWwindow* m_window;
-	static float m_deltaTime;
-	static bool m_focusGUI;
-	static float m_lastX;
-	static float m_lastY;
+	extern unsigned int m_width;
+	extern unsigned int m_height;
 
-private:
-	static float m_lastFrame;
-	static bool m_firstMouse;
+	extern GLFWwindow* m_window;
 
-	static void framebufferSizeCallback(GLFWwindow* window, int width, int height);
-	static void errorCallback(int error, const char* description);
-	static void mouseCallback(GLFWwindow* window, double xposIn, double yposIn);
-	static void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+	extern float m_deltaTime;
+	extern bool m_focusGUI;
+	extern float m_lastX;
+	extern float m_lastY;
+
+	void initialize(unsigned int width, unsigned int height);
+	void updateDeltaTime();
+	void cleanup();
+	void setupGui();
 };
 
